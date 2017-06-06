de8b2ff130df5ed0ef4019288499d35ba6f12d72
Toby the potato <tobyclh@gmail.com>
2017-06-06 15:40:39 +0900

    Add sox and select

--- server_send.c
+++ server_send.c
@@ -2,38 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <pthread.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
-
+#include <sox.h>
 #include <sys/socket.h>
 
-int main() {
-  int N = 10000;
-  unsigned char data[N];
-  memset(data, 0, sizeof(data));
-
+int setup_socket(int port)
+{
   int ss = socket(PF_INET, SOCK_STREAM, 0);
-  if (ss == -1) {
+  if (ss == -1)
+  {
     perror("socket ss");
     exit(1);
   }
 
   struct sockaddr_in addr;
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(50000);
+  addr.sin_port = htons(port);
   addr.sin_addr.s_addr = INADDR_ANY;
 
   bind(ss, (struct sockaddr *)&addr, sizeof(addr));
   listen(ss, 10);
+  return ss;
+}
+
+
+
+int main()
+{
+  size_t n = 48000 * 2 * 5;
+  sox_sample_t *buf = malloc(sizeof(sox_sample_t) * n);
+  sox_init();
+
+  sox_format_t *ft = sox_open_read("default", 0, 0, "pulseaudio");
 
   struct sockaddr_in client_addr;
   socklen_t len = sizeof(struct sockaddr_in);
+  scanf("%d", &myInt);
+  int ss = setup_socket()
   int s = accept(ss, (struct sockaddr *)&client_addr, &len);
   data[0] = 'h';
-  write(s, data, N);
-
+  while (1)
+  {
+    auto m = sox_read(ft, buf, n);
+    assert(m == n);
+    write(s, data, N);
+  }
+  sox_close(ft);
+  sox_quit();
   return 0;
 }
-
