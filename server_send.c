ab80a77d66688a6106039621daa3da975fe47b00
Toby the potato <tobyclh@gmail.com>
2017-06-06 16:07:49 +0900

    Add comment

--- server_send.c
+++ server_send.c
@@ -32,10 +32,10 @@ int setup_socket(int port)
 
 int main()
 {
+  // buffer size 48000Hz * 2ch * 5sec
   size_t n = 48000 * 2 * 5;
   sox_sample_t *buf = malloc(sizeof(sox_sample_t) * n);
   sox_init();
-
   sox_format_t *ft = sox_open_read("default", 0, 0, "pulseaudio");
 
   struct sockaddr_in client_addr;
